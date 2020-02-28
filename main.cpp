ee90a957d477f420499c5321d8d875ae8225b10d
Ranju G.C <ranjugc69@gmail.com>
2020-02-28 22:03:22 +0545

    main

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Hyphen on 2/18/2020.
+//
+
+//#include <MainMenu.h>
+#include "header/Game.h"
+
+int main() {
+    sf::RenderWindow window;
+    MainMenu Menu("PULCHOWK");
+    Menu.Start();
+    //Game game;
+    //game.Setup();
+    //game.run();
+    return 0;
+}
